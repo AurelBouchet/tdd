6832eaff995e9ec2261c1c766e716ea1ad357652
AurelBouchet <aurelien-bouchet@hotmail.fr>
2022-01-08 18:38:33 +0100

    test learning wip

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,12 +1,37 @@
 const assert = require('assert');
 
-function capitalizeFirstLetters(input) {
-  return input.length > 0 ? input[0].toUpperCase() + input.slice(1) : '';
+function capitalizeAllFirstLetters(words) {
+  if (words != '') {
+    let capitalizedLetter = words.split(' ');
+    if (words.length > 1) {
+      for (let i = 0; i < capitalizedLetter.length; i++) {
+        capitalizedLetter[i] =
+          capitalizedLetter[i][0].toUpperCase() + capitalizedLetter[i].slice(1);
+      }
+      return capitalizedLetter.join(' ');
+    } else {
+      return capitalizedLetter.toUpperCase();
+    }
+  } else {
+    return '';
+  }
 }
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
-assert.strictEqual(capitalizeFirstLetters('bonjour'), 'Bonjour');
+// assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
-assert.strictEqual(capitalizeFirstLetters('b'), 'B');
+assert.strictEqual(capitalizeAllFirstLetters('bonjour'), 'Bonjour');
 
-assert.strictEqual(capitalizeFirstLetters(''), '');
+//assert.strictEqual(capitalizeAllFirstLetters('b'), 'B');
+
+assert.strictEqual(capitalizeAllFirstLetters(''), '');
+// //
+
+// function capitalizeAllFirstLetters(words) {
+//   let capitalizedLetter = words.split(' ');
+//   for (let i = 0; i < capitalizedLetter.length; i++) {
+//     capitalizedLetter[i] =
+//       capitalizedLetter[i][0].toUpperCase() + capitalizedLetter[i].slice(1);
+//   }
+//   return capitalizedLetter.join(' ');
+// }
+assert.strictEqual(capitalizeAllFirstLetters('hello world'), 'Hello World');
